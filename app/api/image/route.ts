6b9ce6c5d0eb7117ec6a0dc7c5ca3f5e6ef5500a
Abbas khalil <khalilabbas389@gmail.com>
2024-10-07 04:20:29 -0700

    again some changes made.

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -36,11 +36,11 @@ import { NextRequest } from "next/server";
       prompt
     )}?width=512&height=512&seed=${randomSeed}&nologo=True`;
 
-    // Fetch the image to ensure it was generated
+
     await fetch(imageUrl);
 
     return NextResponse.json({ imageUrl });
-  } catch (error: any) {
+  } catch (error) {
     console.error("Error creating post:", error);
     return NextResponse.json(
       { msg: "Internal server error", error },
@@ -48,49 +48,4 @@ import { NextRequest } from "next/server";
     );
   }
 }
-// export async function GET(req: NextRequest) {  
-//   try {
-//     // Extract search params from the URL to get the image ID
-//     const { searchParams } = new URL(req.url);
-//     const imageId = searchParams.get("id");
-
-//     if (!imageId || typeof imageId !== "string" || imageId.trim() === "") {
-//       return NextResponse.json(
-//         { error: true, message: "Invalid image ID provided" },
-//         { status: 400 }
-//       );
-//     }
-
-//     // Fetch the image data from the Post model using Prisma
-//     const post = await prisma.post.findUnique({
-//       where: {
-//         id: imageId,
-//       },
-//     });
-
-//     console.log("post id ", post)
-
-//     if (!post) {
-//       return NextResponse.json(
-//         { error: true, message: "Image not found" },
-//         { status: 404 }
-//       );
-//     }
-
-//     // Return the image data including the URL, prompt, and seed
-//     return NextResponse.json({ post }, { status: 200 });
-//   } catch (error: any) {
-//     // Log the error for debugging purposes
-//     console.error("Error during GET request:", error);
-
-//     // Return a 500 internal server error response
-//     return NextResponse.json(
-//       { error: true, message: "Internal server error", details: error.message },
-//       { status: 500 }
-//     );
-//   }
-// }
-
-// hey i want the command through which we sa see the local db store on in browser that command i gues start from npm and the rest i forgot
-
 export { POST };
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
 
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Provider from "./Provider";
